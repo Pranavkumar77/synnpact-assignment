2afb2ad6ba2bb0eebe61e0c529823fd6e4bf3921
Pranavkumar Suresh Patil <60297786+Pranavkumar77@users.noreply.github.com>
2023-10-05 00:11:19 +0530

    Update InfiniteScroll.jsx

--- src/InfiniteScroll.jsx
+++ src/InfiniteScroll.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Article from './Article'; // Import the Article component
+import Article from './Article';
 
 const InfiniteScroll = () => {
   const [articles, setArticles] = useState([]);
